fd3a36593560177aa7165d3ca6f057333d99e92d
student <student@hubu.edu.cn>
2019-04-08 17:24:31 +0800

    传个图

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,34 +3,41 @@
   <i-notice-bar icon="systemprompt" loop>
     {{notice}}举行音乐盛典
     </i-notice-bar>
+   <img class="userinfo-avatar" src="cloud://hahaha-4f10fd.6861-hahaha-4f10fd/music.png" background-size="cover" />
    <i-grid i-class="no-border">
     <i-grid-item v-for="item in grids" :key="item"  i-class="no-border">
      <i-grid-lable>{{item}}</i-grid-lable>
     </i-grid-item>
    </i-grid>
-    <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
-      <img class="userinfo-avatar" src="/static/images/user.png" background-size="cover" />
-
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
-      </div>
-    </div>
-
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
-      </div>
-    </div>
-
-   
-    
-    <div class="all">
-        <div class="left">
-        </div>
-        <div class="right">
-        </div>
-    </div>
+    <i-panel title="也许你想了解的音乐知识">
+      <view class="top-padding">
+      <i-card title="良品铺子" extra="零食" thumb="cloud://hahaha-4f10fd.6861-hahaha-4f10fd/music.png">
+        <view slot="content">好多不错的零食</view>
+        <view slot="footer">后街1-22</view>
+      </i-card>
+      <view class="top-padding"></view>
+      <i-card title="卡片标题" i-class="top-padding" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">内容不错</view>
+        <view slot="footer">尾部内容</view>
+      </i-card>
+      <view class="top-padding"></view>
+      <i-card title="卡片标题" i-class="top-padding" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">内容不错</view>
+        <view slot="footer">尾部内容</view>
+      </i-card>
+      <view class="top-padding"></view>
+      <i-card title="卡片标题" i-class="top-padding" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">内容不错</view>
+        <view slot="footer">尾部内容</view>
+      </i-card>
+      <view class="top-padding"></view>
+      <i-card title="卡片标题" i-class="top-padding" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">内容不错</view>
+        <view slot="footer">尾部内容</view>
+      </i-card>
+       <view class="top-padding"></view>
+    </view>
+    </i-panel>
   </div>
 </template>
 
