65d15060eba6b40a099a508a7edc96e9589d38d5
HarryPotter66 <1150513788@qq.com>
2019-04-10 11:31:54 +0800

    4.10

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,20 +2,20 @@
   <div @click="clickHandle">
   <i-notice-bar icon="systemprompt" loop>
     {{notice}}举行音乐盛典
-    </i-notice-bar>
-   <img class="userinfo-avatar" src="cloud://hahaha-4f10fd.6861-hahaha-4f10fd/music.png" background-size="cover" />
-   <i-grid i-class="no-border">
+      </i-notice-bar>
+    <i-grid i-class="no-border">
     <i-grid-item v-for="item in grids" :key="item"  i-class="no-border">
      <i-grid-lable>{{item}}</i-grid-lable>
     </i-grid-item>
    </i-grid>
     <i-panel title="也许你想了解的音乐知识">
       <view class="top-padding">
+       <view  v-for="item in mes" :key='item' class="top-padding">
       <i-card title="五线谱" extra="doremi" thumb="cloud://hahaha-4f10fd.6861-hahaha-4f10fd/music.png">
         <view slot="content"></view>
         <view slot="footer">what is rules in chrom？</view>
       </i-card>
-      <view class="top-padding"></view>
+     </view>
       <i-card title="卡片标题" i-class="top-padding" extra="额外内容" thumb="cloud://hahaha-4f10fd.6861-hahaha-4f10fd/music.png">
         <view slot="content">内容不错</view>
         <view slot="footer">尾部内容</view>
@@ -47,6 +47,11 @@ import card from '@/components/card'
 export default {
   data () {
     return {
+      userInfo: {
+        nickName: 'mpvue',
+        avatarUrl: 'cloud://hahaha-4f10fd.6861-hahaha-4f10fd/6a2c8d019b4f7f0ced93b265d1af2158.gif'
+      },
+      mes: [],
       notice: '2019年4月3日',
       grids: ['摇滚', '爵士', '古典', '流行', '民谣']
     }
@@ -71,7 +76,16 @@ export default {
   },
 
   created () {
-    // let app = getApp()
+    const db = wx.cloud.database({ env: 'hahaha-4f10fd' })
+    db.collection('know').get().then(
+      res => {
+        console.log(res)
+        this.mes = res.data
+      }
+    )
+    wx.cloud.callFunction({ name: 'me' }).then(
+      res => { console.log(res) }
+    )
   }
 }
 </script>
